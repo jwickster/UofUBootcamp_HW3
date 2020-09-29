564c69a4da0e73f414d0115234df70e12016e653
Jwickster <joshwickster@gmail.com>
2020-09-28 18:39:08 -0600

    add edit

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Declare numbers, letters and special chars. lower case will be done with toLower method
+var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters  = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "~", "<", ">", "(", ")"];
+
+
+function createPassword(){
+
+}
