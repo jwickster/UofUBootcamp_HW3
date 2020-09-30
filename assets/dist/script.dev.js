bf70f9f60f7cad2fb2159fa73d48027c1e6967e3
Jwickster <joshwickster@gmail.com>
2020-09-30 10:34:54 -0600

    add comments

--- assets/dist/script.dev.js
+++ assets/dist/script.dev.js
@@ -1,24 +1,19 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+"use strict";
 
-// Write password to the #password input
+// Assignment Code
+var generateBtn = document.querySelector("#generate"); // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+} // Add event listener to generate button
 
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); //Declare numbers, letters and special chars. lower case will be done with 'toLower' method
 
-//Declare numbers, letters and special chars. lower case will be done with 'toLower' method
 var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "~", "<", ">", "(", ")"];
 
-
-function createPassword() {
-
-}
+function createPassword() {}
